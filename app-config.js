93fe3f7d3440f199b3e84311be7ffd5744907c0b
FateXVoid <165581946+FateXVoid@users.noreply.github.com>
2026-07-22 00:54:04 +0700

    Add files via upload

--- app-config.js
+++ app-config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   supabaseUrl: "https://qpkcevmdfbjspiidgxgg.supabase.co",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwa2Nldm1kZmJqc3BpaWRneGdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2Mjk5NDgsImV4cCI6MjEwMDIwNTk0OH0.7IkNtnkcgo1GllsLp1uEc0Muhz-tH-fn5wYvOdFafGA",
+  supabaseAnonKey: "sb_publishable_dBMoMtjSCw9P3ZHhS8zJfg_z9v1tWD9",
 
   promptPayName: "นายกรณพัฒน์ สายวงค์สอน",
   promptPayNumber: "095-721-3642",
