5778e6a22ec45d0a4bca59d8765ebfe1611fd260
FateXVoid <165581946+FateXVoid@users.noreply.github.com>
2026-07-21 23:30:44 +0700

    Delete app-config.js

--- app-config.js
+++
@@ -1,4 +0,0 @@
-window.APP_CONFIG = {
-  supabaseUrl: "https://qpkcevmdfbjspiidgxgg.supabase.co",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwa2Nldm1kZmJqc3BpaWRneGdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2Mjk5NDgsImV4cCI6MjEwMDIwNTk0OH0.7IkNtnkcgo1GllsLp1uEc0Muhz-tH-fn5wYvOdFafGA"
-};
