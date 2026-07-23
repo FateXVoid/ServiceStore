d1bc6eacb09fc7adaa069a6d338536c300f723ef
FateXVoid <165581946+FateXVoid@users.noreply.github.com>
2026-07-23 22:04:18 +0700

    Add files via upload

---
+++ service-store/app-config.js
@@ -0,0 +1,12 @@
+window.APP_CONFIG = {
+  supabaseUrl: "https://qpkcevmdfbjspiidgxgg.supabase.co",
+  supabaseAnonKey: "sb_publishable_dBMoMtjSCw9P3ZHhS8zJfg_z9v1tWD9",
+
+  bankName: "ธนาคารกสิกรไทย",
+  bankAccountName: "นายกรณพัฒน์ สายวงค์สอน",
+  bankAccountNumber: "179-8-83569-2",
+  bankQrImage: "promptpay-qr.png",
+
+  trueMoneyName: "นายกรณพัฒน์ สายวงค์สอน",
+  trueMoneyNumber: "095-721-3642"
+};
